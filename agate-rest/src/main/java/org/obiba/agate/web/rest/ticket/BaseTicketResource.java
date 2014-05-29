fd6d9f6db6e7ebff27c8cae7f719ffaff6ea6372
Yannick Marcon <yannick.marcon@obiba.org>
2014-05-29 16:43:33 -0400

    Code cleaning

--- agate-rest/src/main/java/org/obiba/agate/web/rest/ticket/BaseTicketResource.java
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/ticket/BaseTicketResource.java
@@ -15,9 +15,6 @@ import javax.ws.rs.ForbiddenException;
 
 import org.obiba.agate.service.ApplicationService;
 
-/**
- * Created by ymarcon on 29/05/14.
- */
 public class BaseTicketResource {
 
   @Inject
