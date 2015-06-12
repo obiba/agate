cd89693c8e50c79250e4e8f4fc0ce9c996e2888f
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-06-12 13:54:10 -0400

    fix-user-app-validation - validate user for application

---
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/security/AuthorizationValidator.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2015 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.agate.web.rest.security;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.ForbiddenException;
+
+import org.apache.shiro.subject.Subject;
+import org.obiba.agate.domain.Group;
+import org.obiba.agate.domain.User;
+import org.obiba.agate.domain.UserStatus;
+import org.obiba.agate.service.ApplicationService;
+import org.obiba.agate.service.UserService;
+import org.obiba.shiro.authc.HttpAuthorizationToken;
+import org.obiba.shiro.realm.ObibaRealm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationValidator {
+
+  private static final Logger log = LoggerFactory.getLogger(AuthorizationValidator.class);
+
+  @Inject
+  private UserService userService;
+
+  @Inject
+  private ApplicationService applicationService;
+
+  public String validateApplication(String appAuthHeader) {
+    if (appAuthHeader == null) throw new ForbiddenException();
+
+    HttpAuthorizationToken token = new HttpAuthorizationToken(ObibaRealm.APPLICATION_AUTH_SCHEMA, appAuthHeader);
+    String appName = token.getUsername();
+
+    validateApplicationParameters(appName, new String(token.getPassword()));
+
+    return appName;
+  }
+
+  public void validateRealm(HttpServletRequest servletRequest, User user, Subject subject) {
+    // check that authentication realm is the expected one as specified in user profile
+    if(!subject.getPrincipals().getRealmNames().contains(user.getRealm())) {
+      log.info("Authentication failure of user '{}' at ip: '{}': unexpected realm '{}'", user.getName(),
+        servletRequest.getRemoteAddr(), subject.getPrincipals().getRealmNames().iterator().next());
+      throw new ForbiddenException();
+    }
+  }
+
+  /**
+     * Check user exists and has the right status.
+     *
+     * @param servletRequest
+     * @param username
+     * @param user
+     */
+
+  public void validateUser(HttpServletRequest servletRequest, String username, User user) {
+    if(user == null) {
+      log.warn("Not a registered user '{}' at ip: '{}'", username, servletRequest.getRemoteAddr());
+      throw new ForbiddenException();
+    } else if(user.getStatus() != UserStatus.ACTIVE) {
+      log.warn("Not an active user '{}': status is '{}'", username, user.getStatus());
+      throw new ForbiddenException();
+    }
+  }
+
+  public void validateApplication(HttpServletRequest servletRequest, User user, String appName) {
+    // check application
+    if(!user.hasApplication(appName) && user.getGroups().stream().noneMatch(g -> {
+      Group group = userService.findGroup(g);
+      return group != null && group.hasApplication(appName);
+    })) {
+      log.info("Application '{}' not allowed for user '{}' at ip: '{}'", appName, user.getName(),
+        servletRequest.getRemoteAddr());
+      throw new ForbiddenException();
+    }
+  }
+
+  /**
+   * Check application credentials and set current application name.
+   *
+   * @param name
+   * @param key
+   */
+  public void validateApplicationParameters(String name, String key) {
+    if(!applicationService.isValid(name, key)) throw new ForbiddenException();
+  }
+
+}
