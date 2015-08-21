eb88ec9278993c1b0b2844f783d592f6ba00f498
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-08-21 15:36:32 -0400

    Added created and last login date

--- agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUserResource.java
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUserResource.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -24,6 +25,7 @@ import org.obiba.agate.service.ApplicationService;
 import org.obiba.agate.service.NoSuchUserException;
 import org.obiba.agate.service.UserService;
 import org.obiba.agate.web.model.Dtos;
+import org.obiba.shiro.realm.ObibaRealm;
 import org.obiba.web.model.AuthDtos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +35,7 @@ import com.google.common.base.Strings;
 
 @Component
 @Path("/application/{id}/user/{username}")
-public class ApplicationUserResource {
+public class ApplicationUserResource extends ApplicationAwareResource {
   private static final Logger log = LoggerFactory.getLogger(ApplicationUserResource.class);
 
   @Inject
@@ -47,7 +49,9 @@ public class ApplicationUserResource {
 
   @GET
   public AuthDtos.SubjectDto get(@PathParam("id") String id, @PathParam("username") String username,
-    @QueryParam("group") String group) {
+    @HeaderParam(ObibaRealm.APPLICATION_AUTH_HEADER) String authHeader, @QueryParam("group") String group) {
+
+    validateApplication(authHeader);
     Application application = applicationService.getApplication(id);
 
     List<User> users = Strings.isNullOrEmpty(group)
--- agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUsersResource.java
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUsersResource.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -23,19 +24,18 @@ import org.obiba.agate.domain.Application;
 import org.obiba.agate.domain.User;
 import org.obiba.agate.service.ApplicationService;
 import org.obiba.agate.service.UserService;
-import org.obiba.agate.web.model.Agate;
 import org.obiba.agate.web.model.Dtos;
+import org.obiba.shiro.realm.ObibaRealm;
 import org.obiba.web.model.AuthDtos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableList;
 
 @Component
 @Path("/application/{id}/users")
-public class ApplicationUsersResource {
+public class ApplicationUsersResource extends ApplicationAwareResource {
   private static final Logger log = LoggerFactory.getLogger(ApplicationUsersResource.class);
 
   @Inject
@@ -48,8 +48,9 @@ public class ApplicationUsersResource {
   protected UserService userService;
 
   @GET
-  public List<AuthDtos.SubjectDto> get(@PathParam("id") String id, @QueryParam("group") String group) {
-    ImmutableList.Builder<Agate.UserDto> builder = ImmutableList.builder();
+  public List<AuthDtos.SubjectDto> get(@PathParam("id") String id,
+    @HeaderParam(ObibaRealm.APPLICATION_AUTH_HEADER) String authHeader, @QueryParam("group") String group) {
+    validateApplication(authHeader);
     Application application = applicationService.getApplication(id);
     List<User> users = Strings.isNullOrEmpty(group)
       ? userService.findActiveUsersByApplication(application.getName())
