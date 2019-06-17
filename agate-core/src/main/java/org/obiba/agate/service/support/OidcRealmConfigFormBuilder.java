e449a00f783a659d41043aa21e4ee00bc49ba08f
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2019-06-17 16:07:31 -0400

    Bugfix - wrong OIDCConfiguration bean field name.

--- agate-core/src/main/java/org/obiba/agate/service/support/OidcRealmConfigFormBuilder.java
+++ agate-core/src/main/java/org/obiba/agate/service/support/OidcRealmConfigFormBuilder.java
@@ -23,7 +23,7 @@ public class OidcRealmConfigFormBuilder extends BaseRealmConfigFormBuilder {
       "      \"title\": \"t(realm.oidc.client-id)\"," +
       "      \"description\": \"t(realm.oidc.client-id-help)\"" +
       "    }," +
-      "    \"clientSecret\": {" +
+      "    \"secret\": {" +
       "      \"type\": \"string\"," +
       "      \"title\": \"t(realm.oidc.client-secret)\"," +
       "      \"format\": \"password\"," +
@@ -66,7 +66,7 @@ public class OidcRealmConfigFormBuilder extends BaseRealmConfigFormBuilder {
       "  }," +
       "  \"required\": [" +
       "    \"clientId\"," +
-      "    \"clientSecret\"," +
+      "    \"secret\"," +
       "    \"discoveryURI\"" +
       "  ]" +
       "}";
@@ -76,7 +76,7 @@ public class OidcRealmConfigFormBuilder extends BaseRealmConfigFormBuilder {
       "  \"type\": \"section\"," +
       "  \"items\": [" +
       "      \"clientId\"," +
-      "      \"clientSecret\"," +
+      "      \"secret\"," +
       "      \"discoveryURI\"," +
       "      \"providerUrl\"," +
       "      \"scope\"," +
