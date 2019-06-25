58abef3165d16c642ee61e1f8d8cdc65c2af7a8d
Yannick Marcon <yannick.marcon@obiba.org>
2019-06-25 15:35:34 +0200

    Use the always not null public url to build the OIDC configuration

--- agate-core/src/main/java/org/obiba/agate/oidc/OIDCConfigurationFilter.java
+++ agate-core/src/main/java/org/obiba/agate/oidc/OIDCConfigurationFilter.java
@@ -52,32 +52,18 @@ public class OIDCConfigurationFilter extends OncePerRequestFilter {
   }
 
   private JSONObject getOIDCConfiguration(HttpServletRequest request) throws JSONException {
-    String baseUrl = getBaseUrl(request);
+    String baseUrl = configurationService.getPublicUrl();
     JSONObject oidcConfig = new JSONObject();
     oidcConfig.put("issuer", tokenUtils.getIssuerID());
     oidcConfig.put("authorization_endpoint", baseUrl + "/ws/oauth2/authorize");
     oidcConfig.put("token_endpoint", baseUrl + "/ws/oauth2/token");
     oidcConfig.put("userinfo_endpoint", baseUrl + "/ws/oauth2/userinfo");
-    oidcConfig.put("scopes_supported", new String[]{ "openid", "email", "profile" });
-    oidcConfig.put("id_token_signing_alg_values_supported", new String[]{ tokenUtils.getSignatureAlgorithm() });
-    oidcConfig.put("response_types_supported", new String[]{ "code" });
-    oidcConfig.put("subject_types_supported", new String[]{ "public" });
+    oidcConfig.put("scopes_supported", new String[]{"openid", "email", "profile"});
+    oidcConfig.put("id_token_signing_alg_values_supported", new String[]{tokenUtils.getSignatureAlgorithm()});
+    oidcConfig.put("response_types_supported", new String[]{"code"});
+    oidcConfig.put("subject_types_supported", new String[]{"public"});
     oidcConfig.put("jwks_uri", baseUrl + "/ws/oauth2/certs");
     return oidcConfig;
-  }
-
-  private String getBaseUrl( HttpServletRequest request ) {
-    if (configurationService.getConfiguration().hasPublicUrl())
-      return configurationService.getConfiguration().getPublicUrl();
-    if ( ( request.getServerPort() == 80 ) ||
-      ( request.getServerPort() == 443 ) )
-      return request.getScheme() + "://" +
-        request.getServerName() +
-        request.getContextPath();
-    else
-      return request.getScheme() + "://" +
-        request.getServerName() + ":" + request.getServerPort() +
-        request.getContextPath();
   }
 
 }
