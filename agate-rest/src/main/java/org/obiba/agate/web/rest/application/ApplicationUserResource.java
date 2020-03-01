5c41342dbaf02d29935ba566da7c612ff5b1f3a3
Yannick Marcon <yannick.marcon@obiba.org>
2020-03-01 09:27:01 +0100

    #3799 An application can query only its own users

--- agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUserResource.java
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUserResource.java
@@ -10,18 +10,8 @@
 
 package org.obiba.agate.web.rest.application;
 
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-
-import org.obiba.agate.domain.Application;
+import com.google.common.base.Strings;
 import org.obiba.agate.domain.User;
-import org.obiba.agate.service.ApplicationService;
 import org.obiba.agate.service.NoSuchUserException;
 import org.obiba.agate.service.UserService;
 import org.obiba.agate.web.model.Dtos;
@@ -31,7 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.google.common.base.Strings;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import java.util.List;
 
 @Component
 @Path("/application/{id}/user/{username}")
@@ -42,21 +34,16 @@ public class ApplicationUserResource extends ApplicationAwareResource {
   private Dtos dtos;
 
   @Inject
-  private ApplicationService applicationService;
-
-  @Inject
   protected UserService userService;
 
   @GET
   public AuthDtos.SubjectDto get(@PathParam("id") String id, @PathParam("username") String username,
-    @HeaderParam(ObibaRealm.APPLICATION_AUTH_HEADER) String authHeader, @QueryParam("group") String group) {
-
+                                 @HeaderParam(ObibaRealm.APPLICATION_AUTH_HEADER) String authHeader, @QueryParam("group") String group) {
     validateApplication(authHeader);
-    Application application = applicationService.getApplication(id);
-
+    if (!getApplicationName().equals(id)) throw new BadRequestException("Application can only query its own users");
     List<User> users = Strings.isNullOrEmpty(group)
-      ? userService.findActiveUserByApplication(username, application.getName())
-      : userService.findActiveUserByApplicationAndGroup(username, application.getName(), group);
+      ? userService.findActiveUserByApplication(username, getApplicationName())
+      : userService.findActiveUserByApplicationAndGroup(username, getApplicationName(), group);
 
     if (users.isEmpty()) NoSuchUserException.withName(username);
     return dtos.asDto(users.get(0), true);
--- agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUsersResource.java
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/application/ApplicationUsersResource.java
@@ -10,19 +10,8 @@
 
 package org.obiba.agate.web.rest.application;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-
-import org.obiba.agate.domain.Application;
+import com.google.common.base.Strings;
 import org.obiba.agate.domain.User;
-import org.obiba.agate.service.ApplicationService;
 import org.obiba.agate.service.UserService;
 import org.obiba.agate.web.model.Dtos;
 import org.obiba.shiro.realm.ObibaRealm;
@@ -31,7 +20,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.google.common.base.Strings;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Path("/application/{id}/users")
@@ -42,20 +34,16 @@ public class ApplicationUsersResource extends ApplicationAwareResource {
   private Dtos dtos;
 
   @Inject
-  private ApplicationService applicationService;
-
-  @Inject
   protected UserService userService;
 
   @GET
   public List<AuthDtos.SubjectDto> get(@PathParam("id") String id,
-    @HeaderParam(ObibaRealm.APPLICATION_AUTH_HEADER) String authHeader, @QueryParam("group") String group) {
+                                       @HeaderParam(ObibaRealm.APPLICATION_AUTH_HEADER) String authHeader, @QueryParam("group") String group) {
     validateApplication(authHeader);
-    Application application = applicationService.getApplication(id);
+    if (!getApplicationName().equals(id)) throw new BadRequestException("Application can only query its own users");
     List<User> users = Strings.isNullOrEmpty(group)
-      ? userService.findActiveUsersByApplication(application.getName())
-      : userService.findActiveUsersByApplicationAndGroup(application.getName(), group);
-
+      ? userService.findActiveUsersByApplication(getApplicationName())
+      : userService.findActiveUsersByApplicationAndGroup(getApplicationName(), group);
 
     return users.stream().map(u -> dtos.asDto(u, true)).collect(Collectors.toList());
   }
