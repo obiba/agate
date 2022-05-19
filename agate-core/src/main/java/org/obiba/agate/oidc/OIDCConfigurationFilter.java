eb53418d103b313a5afbc91e529d43bc07ab2c3a
Yannick Marcon <yannick.marcon@obiba.org>
2022-05-19 13:16:31 +0200

    #480 Try to guess the host for the well-known openid config

--- agate-core/src/main/java/org/obiba/agate/oidc/OIDCConfigurationFilter.java
+++ agate-core/src/main/java/org/obiba/agate/oidc/OIDCConfigurationFilter.java
@@ -1,5 +1,7 @@
 package org.obiba.agate.oidc;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.obiba.agate.service.ConfigurationService;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 /**
  * Implements the well-known entry point that provides the settings of OpenID Connect.
@@ -51,20 +54,40 @@ public class OIDCConfigurationFilter extends OncePerRequestFilter {
   }
 
   private JSONObject getOIDCConfiguration(HttpServletRequest request) throws JSONException {
-    // TODO be more more flexible with entry point as agate could be accessed from different host name
-    // request.getRequestURI();
-    String baseUrl = configurationService.getPublicUrl();
+    // Be flexible with entry point as agate could be accessed from different host name
+    Map<String, String> queryMap = parseQuery(request.getQueryString());
+    String host = request.getHeader("Host");
+    if (queryMap.containsKey("host"))
+      host = queryMap.get("host");
+    if (Strings.isNullOrEmpty(host))
+      host = configurationService.getPublicUrl();
     JSONObject oidcConfig = new JSONObject();
     oidcConfig.put("issuer", tokenUtils.getIssuerID());
-    oidcConfig.put("authorization_endpoint", baseUrl + "/ws/oauth2/authorize");
-    oidcConfig.put("token_endpoint", baseUrl + "/ws/oauth2/token");
-    oidcConfig.put("userinfo_endpoint", baseUrl + "/ws/oauth2/userinfo");
+    oidcConfig.put("authorization_endpoint", host + "/ws/oauth2/authorize");
+    oidcConfig.put("token_endpoint", host + "/ws/oauth2/token");
+    oidcConfig.put("userinfo_endpoint", host + "/ws/oauth2/userinfo");
     oidcConfig.put("scopes_supported", new String[]{"openid", "email", "profile"});
     oidcConfig.put("id_token_signing_alg_values_supported", new String[]{tokenUtils.getSignatureAlgorithm()});
     oidcConfig.put("response_types_supported", new String[]{"code"});
     oidcConfig.put("subject_types_supported", new String[]{"public"});
-    oidcConfig.put("jwks_uri", baseUrl + "/ws/oauth2/certs");
+    oidcConfig.put("jwks_uri", host + "/ws/oauth2/certs");
     return oidcConfig;
+  }
+
+  private Map<String, String> parseQuery(String query) {
+    Map<String, String> queryMap = Maps.newHashMap();
+    if (Strings.isNullOrEmpty(query)) return queryMap;
+
+    for (String pair : query.split("&")) {
+      int idxOfEqual = pair.indexOf("=");
+
+      if (idxOfEqual > 0) {
+        String key = pair.substring(0, idxOfEqual);
+        String value = pair.substring(idxOfEqual + 1);
+        queryMap.put(key, value);
+      }
+    }
+    return queryMap;
   }
 
 }
