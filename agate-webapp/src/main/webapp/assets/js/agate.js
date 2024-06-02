c2318e91e62d8a498639c6961b34a4e5cb1ca34c
Yannick Marcon <yannick.marcon@obiba.org>
2024-06-02 09:57:46 +0200

    fix: encode redirect query parameter in js

--- agate-webapp/src/main/webapp/assets/js/agate.js
+++ agate-webapp/src/main/webapp/assets/js/agate.js
@@ -39,7 +39,7 @@ var agatejs = (function() {
           const q = new URLSearchParams(window.location.search);
           if (q.get('redirect')) {
             // verify redirect
-            redirect = normalizeUrl('/check?redirect=') + q.get('redirect');
+            redirect = normalizeUrl('/check?redirect=') + encodeURIComponent(q.get('redirect'));
           }
           window.location = redirect;
         })
@@ -106,7 +106,7 @@ var agatejs = (function() {
             let values = {};
             const q = new URLSearchParams(window.location.search);
             if (q.get('redirect')) {
-              redirect = normalizeUrl('/check?redirect=') + q.get('redirect');
+              redirect = normalizeUrl('/check?redirect=') + encodeURIComponent(q.get('redirect'));
               window.location = redirect;
             } else if (realmField) {
               redirect = normalizeUrl('/just-registered');
@@ -201,7 +201,7 @@ var agatejs = (function() {
             let redirect = normalizeUrl('/');
             const q = new URLSearchParams(window.location.search);
             if (q.get('redirect')) {
-              redirect = normalizeUrl('/check?redirect=') + q.get('redirect');
+              redirect = normalizeUrl('/check?redirect=') + encodeURIComponent(q.get('redirect'));
             }
             window.location = redirect;
           })
