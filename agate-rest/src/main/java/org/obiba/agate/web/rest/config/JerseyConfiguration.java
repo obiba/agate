118a925a4a4da4bccdc1d1155006729db75b6277
Yannick Marcon <yannick.marcon@obiba.org>
2024-12-22 18:17:01 +0100

    fix: ensure Referer header is provided or check allowed user agent

--- agate-rest/src/main/java/org/obiba/agate/web/rest/config/JerseyConfiguration.java
+++ agate-rest/src/main/java/org/obiba/agate/web/rest/config/JerseyConfiguration.java
@@ -37,7 +37,7 @@ public class JerseyConfiguration extends ResourceConfig {
     ///register(LoggingFilter.class);
     register(AuthenticationInterceptor.class);
     register(AuditInterceptor.class);
-    register(new CSRFInterceptor(environment.acceptsProfiles(Profiles.of(Constants.SPRING_PROFILE_PRODUCTION)), environment.getProperty("csrf.allowed", "")));
+    register(new CSRFInterceptor(environment.acceptsProfiles(Profiles.of(Constants.SPRING_PROFILE_PRODUCTION)), environment.getProperty("csrf.allowed", ""), environment.getProperty("csrf.allowed-agents", "")));
     // validation errors will be sent to the client
     property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
   }
