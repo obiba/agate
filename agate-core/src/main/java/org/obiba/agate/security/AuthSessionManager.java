995d40a47b66b9abb323db933fac1bd4a3f26699
Yannick Marcon <yannick.marcon@obiba.org>
2025-02-16 17:36:22 +0100

    chore: added debug messages

--- agate-core/src/main/java/org/obiba/agate/security/AuthSessionManager.java
+++ agate-core/src/main/java/org/obiba/agate/security/AuthSessionManager.java
@@ -10,9 +10,30 @@
 
 package org.obiba.agate.security;
 
+import org.obiba.oidc.OIDCSession;
 import org.obiba.oidc.utils.DefaultOIDCSessionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthSessionManager extends DefaultOIDCSessionManager {
+  private final Logger log = LoggerFactory.getLogger(AuthSessionManager.class);
+  @Override
+  public void saveSession(OIDCSession session) {
+    log.debug("Saving session {}", session.getId());
+    super.saveSession(session);
+  }
+
+  @Override
+  public boolean hasSession(String client) {
+    log.debug("Checking if session {} exists", client);
+    return super.hasSession(client);
+  }
+
+  @Override
+  public OIDCSession getSession(String client) {
+    log.debug("Getting session {}", client);
+    return super.getSession(client);
+  }
 }
