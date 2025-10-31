50794102d08e75c47568d90420a0e2fa62606282
Yannick Marcon <yannick.marcon@obiba.org>
2025-10-31 14:04:24 +0100

    feat: exec java process that launches agate

--- agate-dist/src/main/rpm/start.sh
+++ agate-dist/src/main/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$JAVA $JAVA_ARGS -cp "${AGATE_HOME}/conf:${AGATE_DIST}/webapp/WEB-INF/classes:${AGATE_DIST}/webapp/WEB-INF/lib/*" -DAGATE_HOME=${AGATE_HOME} -DAGATE_DIST=${AGATE_DIST} -DAGATE_LOG=${AGATE_LOG} org.obiba.agate.Application $AGATE_ARGS
+exec $JAVA $JAVA_ARGS -cp "${AGATE_HOME}/conf:${AGATE_DIST}/webapp/WEB-INF/classes:${AGATE_DIST}/webapp/WEB-INF/lib/*" -DAGATE_HOME=${AGATE_HOME} -DAGATE_DIST=${AGATE_DIST} -DAGATE_LOG=${AGATE_LOG} org.obiba.agate.Application $AGATE_ARGS
